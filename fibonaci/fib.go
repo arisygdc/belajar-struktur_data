ecf214f15b275e05fa5a633ea868e6b6d4c6c930
arisygdc <arisycso@gmail.com>
2021-12-13 19:08:19 +0700

    fibonacci

---
+++ fibonaci/fib.go
@@ -0,0 +1,26 @@
+package fibonaci
+
+type Fib struct {
+	cache map[int]float64
+}
+
+func (fib Fib) fibRec(n int) (result float64) {
+	if val, ok := fib.cache[n]; ok {
+		result = val
+		return
+	} else if n <= 2 {
+		result = 1
+	} else {
+		result = fib.fibRec(n-1) + fib.fibRec(n-2)
+	}
+	fib.cache[n] = result
+	return
+}
+
+func Run(n int) float64 {
+	fib := Fib{
+		cache: make(map[int]float64),
+	}
+	result := fib.fibRec(n)
+	return result
+}
